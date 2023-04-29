7317729fc194d5376c39e6269a0eba0d69b75a75
AleksandrBausher <bausheraleksandr@gmail.com>
2023-04-29 11:23:54 -0400

    connected to json file, and got the posted data in the file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const db = require("./db/db.json");
+
 const path = require("path");
 
 const app = express();
@@ -7,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.static("Develop/public"));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
@@ -16,6 +18,37 @@ app.get("/", (req, res) => {
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "public/notes.html"))
 );
+
+app.get("/api/notes", (req, res) => {
+  res.json(db);
+});
+
+app.post("/api/notes", (req, res) => {
+  db.push(req.body);
+  fs.writeFile(
+    path.join(__dirname, "db/db.json"),
+    JSON.stringify(db),
+    (error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Successfully added to the file");
+      }
+    }
+  );
+
+  let response;
+
+  if (req.body && req.body.product) {
+    response = {
+      status: "success",
+      data: req.body,
+    };
+    res.status(201).json(response);
+  } else {
+    res.status(400).json("Request body must at least contain some data");
+  }
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
